24caa94c886c08b172faa31ff30dc937e01849a4
SPiCaRiA <11176415+SPiCaRiA@users.noreply.github.com>
2025-08-27 16:23:19 -0400

    fix polkit

--- koxia/services/polkit.scm
+++ koxia/services/polkit.scm
@@ -1,6 +1,9 @@
 (define-module (koxia services authentication)
-  #:use-module (gnu services)
+  #:use-module ((gnu services) #:select (simple-service))
   #:use-module ((gnu services dbus) #:select (polkit-service-type))
+  #:use-module ((guix gexp) #:select (file-union
+                                      file-append))
+  #:use-module ((koxia packages 1password) #:select (1password))
   #:export (polkit-1password-service))
 
 ;;; Commentary:
@@ -10,7 +13,9 @@
 (define polkit-1password
   (file-union
    "polkit-1password"
-   `(("share/polkit-1/actions/com.1password.Password.policy"))))
+   `(("share/polkit-1/actions/com.1password.Password.policy"
+      ,(file-append 1password
+                    "share/polkit-1/actions/com.1password.Password.policy")))))
 
 (define polkit-1password-service
   (simple-service 'polkit-1password
