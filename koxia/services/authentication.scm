0eea90dbbaaf921891c2f435a0d4e5f1a9f6d2a2
SPiCaRiA <11176415+SPiCaRiA@users.noreply.github.com>
2025-09-06 23:59:10 -0400

    many

--- koxia/services/authentication.scm
+++ koxia/services/authentication.scm
@@ -1,23 +1,74 @@
 (define-module (koxia services authentication)
-  #:use-module ((gnu services) #:select (simple-service))
+  #:use-module ((gnu services) #:select (service-type
+                                         simple-service
+                                         service-extension
+                                         privileged-program-service-type))
+  #:use-module ((gnu services configuration)
+                #:select (define-configuration/no-serialization))
   #:use-module ((gnu services dbus) #:select (polkit-service-type))
+  #:use-module ((gnu system) #:select (account-service-type))
+  #:use-module ((gnu system accounts) #:select (user-group))
+  #:use-module ((gnu system privilege) #:select (privileged-program))
   #:use-module ((guix gexp) #:select (file-union
                                       file-append))
-  #:use-module ((koxia packages 1password) #:select (1password))
-  #:export (polkit-1password-service))
+  #:use-module ((guix records) #:select (match-record-lambda))
+  #:use-module ((koxia packages 1password) #:select (1password
+                                                     1password-cli))
+  #:export (1password-configuration
+            1password-service-type))
 
 ;;; Commentary:
 
 ;;; Code:
 
-(define polkit-1password
-  (file-union
-   "polkit-1password"
-   `(("share/polkit-1/actions/com.1password.Password.policy"
-      ,(file-append 1password
-                    "share/polkit-1/actions/com.1password.Password.policy")))))
+;;; 1Password
+;;;
 
-(define polkit-1password-service
-  (simple-service 'polkit-1password
-                  polkit-service-type
-                  (list polkit-1password)))
+(define-configuration/no-serialization 1password-configuration
+  (1password
+   (file-like 1password)
+   "1Password package to use.")
+  (1password-cli
+   (file-like 1password-cli)
+   "1Password CLI package to use.")
+  (user
+   string
+   "User to set for the 1password-cli privileged program."))
+
+(define 1password-groups
+  (list
+   (user-group (name "onepassword"))
+   (user-group (name "onepassword-cli"))))
+
+(define 1password-privileged-programs
+  (match-record-lambda <1password-configuration>
+                       (1password 1password-cli user)
+                       (list
+                        (privileged-program
+                         (program (file-append 1password-cli "/bin/op"))
+                         (setgid? #t)
+                         (user user)
+                         (group "onepassword-cli"))
+                        (privileged-program
+                         (program (file-append 1password
+                                               "/share/1Password/1Password-BrowserSupport"))
+                         (setgid? #t)
+                         (group "onepassword")))))
+
+(define 1password-polkit-policies
+  (match-record-lambda <1password-configuration>
+                       (1password)
+                       (list 1password)))
+
+(define 1password-service-type
+  (service-type
+   (name '1password)
+   (extensions
+    (list (service-extension account-service-type
+                             (const 1password-groups))
+          (service-extension privileged-program-service-type
+                             1password-privileged-programs)
+          (service-extension polkit-service-type
+                             1password-polkit-policies)))
+   (description "Set user groups, setuid programs, and polkit policies for
+1Password.")))
---
+++ koxia/services/kanata.scm
@@ -0,0 +1,40 @@
+(define-module (koxia services kanata)
+  #:use-module ((gnu packages linux) #:select (kmod))
+  #:use-module ((gnu packages rust-apps) #:select (kanata))
+  #:use-module ((gnu services) #:select (service-type
+                                         service-extension))
+  #:use-module ((gnu services base) #:select (udev-service-type
+                                              udev-rule))
+  #:use-module ((gnu services configuration)
+                #:select (define-configuration/no-serialization))
+  #:use-module ((gnu system) #:select (account-service-type))
+  #:use-module ((gnu system accounts) #:select (user-group))
+  #:use-module ((guix gexp) #:select (file-append))
+  #:use-module ((guix records) #:select (match-record-lambda))
+  #:export (kanata-service-type))
+
+;;; Commentary:
+
+;;; Code:
+
+(define kanata-groups
+  (list
+   (user-group (name "uinput") (system? #t))
+   (user-group (name "input") (system? #t))))
+
+(define kanata-udev-rules
+  (udev-rule
+   "99-kanata.rules"
+   "KERNEL==\"uinput\", MODE=\"0660\", GROUP=\"uinput\", OPTIONS+=\"static_node=uinput\""))
+
+(define kanata-service-type
+  (service-type
+   (name 'kanata)
+   (extensions
+    (list (service-extension account-service-type
+                             (const kanata-groups))
+          (service-extension udev-service-type
+                             (const (list kanata-udev-rules)))
+          (service-extension kernel-module-loader-service-type
+                             (const '("uinput")))))
+   (description "Kanata keyboard remapping service.")))
